9c0d98ea6e28a301d0fbf70ba88a4df8bf9e87d3
BryanHogan <bryanhoganme@gmail.com>
2025-01-17 13:26:10 +0100

    svelte added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   trailingSlash: 'never',
-  integrations: [sitemap()],
+  integrations: [sitemap(), svelte()],
 });
