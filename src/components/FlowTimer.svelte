5c051169201cfa7aeea151ec5c9f2fe5a70e9da9
BryanHogan <bryanhoganme@gmail.com>
2025-01-17 16:32:43 +0100

    building count logic

--- src/components/FlowTimer.svelte
+++ src/components/FlowTimer.svelte
@@ -8,14 +8,50 @@
 
     const timeCurrentItem = $state(15);
     const itemList = $state([
-        { name: "Task 1", length: 15 },
-        { name: "Task 2", length: 17 },
+        { name: "Task 1", length: 4 },
+        { name: "Task 2", length: 4 },
         { name: "", length: 18 },
     ]);
     const totalTime = $derived.by(() => 
         itemList.reduce((sum, item) => sum + item.length, 0)
     );
-    const currentActiveItem = $state(0);
+    let currentActiveItem = $state(0);
+    const currentActiveItemName = $derived(itemList[currentActiveItem].name);
+
+    let currentTime = $state(0);
+    let totalTimePassed = $state(0);
+
+    function countTotalTimePassed() {
+        totalTimePassed = 0;
+        setInterval(() => {
+            totalTimePassed = totalTimePassed + 1;
+        }, 1000);
+    }
+
+    function startTimer() {
+        countTotalTimePassed();
+        currentTime = 0;
+        setInterval(() => {
+            currentTime = currentTime + 1;
+            if (currentTime >= itemList[currentActiveItem].length) {
+                console.log("VICTORYYY");
+                if (currentActiveItem < itemList.length) {
+                currentActiveItem = currentActiveItem + 1;
+                currentTime = 0;
+                } else {
+                    finishTimer();
+                }
+            }
+        }, 1000);
+    }
+
+    function finishTimer() {
+        console.log("Finished!");
+    }
+
+    function skipCurrentActiveItem() {
+        currentActiveItem = currentActiveItem + 1;
+    }
 
     function checkIfItemListFull() {
         let itemListLength = itemList.length;
@@ -47,8 +83,11 @@
         <h2>Options</h2>
     </div>
     <div>
-        <h2>Current item active</h2>
-        <p>{timeCurrentItem}</p>
+        <h2>{currentActiveItemName}</h2>
+        <p>Total time passed: {totalTimePassed}</p>
+        <p>{currentTime}</p>
+        <button onclick={startTimer}>Start Timer</button>
+        <button onclick={skipCurrentActiveItem}>Skip Current Item</button>
         <p>{totalTime}</p>
     </div>
     <div>
